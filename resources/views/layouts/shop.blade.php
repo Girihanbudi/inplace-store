a013cddfa4bf9ead2626794271a3b9a7e410cfc7
giri <ghanbudi@gmail.com>
2020-04-26 14:11:00 +0700

    fix shop deploy

--- resources/views/layouts/shop.blade.php
+++ resources/views/layouts/shop.blade.php
@@ -44,22 +44,22 @@
     <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
 
-    <script src="{{ asset('shopresource/winkel/js/jquery.min.js')}}"></script>
-    <script src="{{ asset('shopresource/winkel/js/jquery-migrate-3.0.1.min.js')}}"></script>
-    <script src="{{ asset('shopresource/winkel/js/popper.min.js')}}"></script>
-    <script src="{{ asset('shopresource/winkel/js/bootstrap.min.js')}}"></script>
-    <script src="{{ asset('shopresource/winkel/js/jquery.easing.1.3.js')}}"></script>
-    <script src="{{ asset('shopresource/winkel/js/jquery.waypoints.min.js')}}"></script>
-    <script src="{{ asset('shopresource/winkel/js/jquery.stellar.min.js')}}"></script>
-    <script src="{{ asset('shopresource/winkel/js/owl.carousel.min.js')}}"></script>
-    <script src="{{ asset('shopresource/winkel/js/jquery.magnific-popup.min.js')}}"></script>
-    <script src="{{ asset('shopresource/winkel/js/aos.js')}}"></script>
-    <script src="{{ asset('shopresource/winkel/js/jquery.animateNumber.min.js')}}"></script>
-    <script src="{{ asset('shopresource/winkel/js/bootstrap-datepicker.js')}}"></script>
-    <script src="{{ asset('shopresource/winkel/js/scrollax.min.js')}}"></script>
-    <script src="{{ asset('shopresource/winkel/https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false')}}"></script>
-    <script src="{{ asset('shopresource/winkel/js/google-map.js')}}"></script>
-    <script src="{{ asset('shopresource/winkel/js/main.js')}}"></script>
+    <script src="/shopresource/winkel/js/jquery.min.js"></script>
+    <script src="/shopresource/winkel/js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="/shopresource/winkel/js/popper.min.js"></script>
+    <script src="/shopresource/winkel/js/bootstrap.min.js"></script>
+    <script src="/shopresource/winkel/js/jquery.easing.1.3.js"></script>
+    <script src="/shopresource/winkel/js/jquery.waypoints.min.js"></script>
+    <script src="/shopresource/winkel/js/jquery.stellar.min.js"></script>
+    <script src="/shopresource/winkel/js/owl.carousel.min.js"></script>
+    <script src="/shopresource/winkel/js/jquery.magnific-popup.min.js"></script>
+    <script src="/shopresource/winkel/js/aos.js"></script>
+    <script src="/shopresource/winkel/js/jquery.animateNumber.min.js"></script>
+    <script src="/shopresource/winkel/js/bootstrap-datepicker.js"></script>
+    <script src="/shopresource/winkel/js/scrollax.min.js"></script>
+    <script src="/shopresource/winkel/https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+    <script src="/shopresource/winkel/js/google-map.js"></script>
+    <script src="/shopresource/winkel/js/main.js"></script>
     
   </body>
 </html>
